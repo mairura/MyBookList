4e9f33a01413ba0075f682a783689b5cba20130c
unknown <mairurated@gmail.com>
2022-03-28 17:56:06 +0300

    Added some file

---
+++ app.js
@@ -0,0 +1,25 @@
+//Book Class: Represents a book in the
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+//UI Class: Handle UI Tasks
+class uI {
+  static displayBooks() {
+    const StoredBooks = [
+      { title: "Book One", author: "John Doe", isbn: "345435" },
+      { title: "Book Two", author: "Tome Mairura", isbn: "46765787" },
+    ];
+  }
+}
+
+//Store Class: Handles Storages
+
+//Event: Display books
+
+//Event: Add a book
+
+//Event: Delete a book
